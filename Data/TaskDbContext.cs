811ed9882864b4be6e2b49801b77892f8bae1fb3
Евгений Горбунов <gorbunovea7@gmail.com>
2026-03-25 17:36:54 +0300

    Remove seed tasks

--- Data/TaskDbContext.cs
+++ Data/TaskDbContext.cs
@@ -28,14 +28,6 @@ namespace TaskManager.Data
                 entity.Property(e => e.CreatedDate).HasDefaultValueSql("GETUTCDATE()");
             });
 
-            modelBuilder.Entity<TaskItem>().HasData(
-                new TaskItem { Id = 1, Name = "My 1st task", Description = "1st task description", CategoryId = 1, CreatedDate = new DateTime(2025, 12, 2), DueDate = new DateTime(2026, 12, 2), StatusId = 1, PriorityId = 2 },
-                new TaskItem { Id = 2, Name = "My 2nd task", Description = "2nd task description", CategoryId = 1, CreatedDate = new DateTime(2025, 11, 2), DueDate = new DateTime(2025, 12, 2), StatusId = 1, PriorityId = 2 },
-                new TaskItem { Id = 4, Name = "My 4th task", Description = "4th task description", CategoryId = 2, CreatedDate = new DateTime(2025, 11, 2), DueDate = new DateTime(2025, 12, 2), StatusId = 1, PriorityId = 2 },
-                new TaskItem { Id = 3, Name = "My 3rd task", Description = "3rd task description", CategoryId = 2, CreatedDate = new DateTime(2025, 12, 2), DueDate = new DateTime(2026, 12, 2), StatusId = 1, PriorityId = 2 },
-                new TaskItem { Id = 5, Name = "My 5th task", Description = "5th task description", CategoryId = 3, CreatedDate = new DateTime(2025, 12, 2), DueDate = new DateTime(2026, 12, 2), StatusId = 1, PriorityId = 2 },
-                new TaskItem { Id = 6, Name = "My 6th task", Description = "6th task description", CategoryId = 3, CreatedDate = new DateTime(2025, 11, 2), DueDate = new DateTime(2025, 12, 2), StatusId = 1, PriorityId = 2 });
-
             modelBuilder.Entity<TaskItem>(entity =>
             {
                 entity.Property(e => e.StatusId).HasDefaultValue(1);
---
+++ Migrations/20260325141536_removeSeedTasks.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeSeedTasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "CategoryId", "CreatedDate", "Description", "DueDate", "Name", "PriorityId", "StatusId", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "1st task description", new DateTime(2026, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "My 1st task", 2, 1, 0 },
+                    { 2, 1, new DateTime(2025, 11, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "2nd task description", new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "My 2nd task", 2, 1, 0 },
+                    { 3, 2, new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "3rd task description", new DateTime(2026, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "My 3rd task", 2, 1, 0 },
+                    { 4, 2, new DateTime(2025, 11, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "4th task description", new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "My 4th task", 2, 1, 0 },
+                    { 5, 3, new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "5th task description", new DateTime(2026, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "My 5th task", 2, 1, 0 },
+                    { 6, 3, new DateTime(2025, 11, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "6th task description", new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "My 6th task", 2, 1, 0 }
+                });
+        }
+    }
+}
