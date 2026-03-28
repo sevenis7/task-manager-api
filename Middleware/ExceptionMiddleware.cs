fbc1c1c6a7ac304069d4f00402c9d76974ca17b6
Евгений Горбунов <gorbunovea7@gmail.com>
2026-03-28 20:05:52 +0300

    Added logging in ExceptionMiddleware

--- Middleware/ExceptionMiddleware.cs
+++ Middleware/ExceptionMiddleware.cs
@@ -6,10 +6,12 @@ namespace TaskManager.Middleware
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
 
-        public ExceptionMiddleware(RequestDelegate next)
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -20,6 +22,7 @@ namespace TaskManager.Middleware
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "An unhandled exception occurred");
                 await HandleExceptionAsync(context, ex);
             }
         }
