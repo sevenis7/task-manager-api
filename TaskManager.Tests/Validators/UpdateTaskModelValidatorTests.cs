4fceb310b30d82b1b212990784ef32164fc61977
Евгений Горбунов <gorbunovea7@gmail.com>
2026-04-05 18:45:10 +0300

    chore: restructure solution — move projects to solution root

---
+++ TaskManager.Tests/Validators/UpdateTaskModelValidatorTests.cs
@@ -0,0 +1,9 @@
+﻿using TaskManager.Validators;
+
+namespace TaskManager.Tests.Validators
+{
+    public class UpdateTaskModelValidatorTests
+    {
+        private readonly UpdateTaskModelValidator _validator = new();
+    }
+}
